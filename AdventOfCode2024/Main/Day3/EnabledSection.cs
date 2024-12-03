20166ec030a0820888e0b603e517dbca81769c5d
Kyle Torman <ktorman@cricut.com>
2024-12-02 23:14:16 -0700

    Finished Day 3

---
+++ AdventOfCode2024/Main/Day3/EnabledSection.cs
@@ -0,0 +1,17 @@
+﻿namespace Main.Day3
+{
+    public class EnabledSection
+    {
+        private readonly int _startIndex;
+        private readonly int _endIndex;
+
+        public EnabledSection(int startIndex, int endIndex)
+        {
+            _startIndex = startIndex;
+            _endIndex = endIndex;
+        }
+
+        public bool IsWithinSection(int index) =>
+            _startIndex <= index && _endIndex >= index;
+    }
+}
--- AdventOfCode2024/Main/Program.cs
+++ AdventOfCode2024/Main/Program.cs
@@ -7,7 +7,7 @@ namespace Main
     {
         static async Task Main(string[] args)
         {
-            var day = Day.Two;
+            var day = Day.Three;
             var inputType = InputType.Puzzle;
             var challengePart = ChallengePart.Two;
             var challenge = Challenges.First(c => c.Day == day);
